56241d6a1455e20848be92eebb6e50707ee53af9
Nyakio-Eva <evalkariuki@gmail.com>
2025-09-28 18:31:13 +0300

    add frontend link in cors

--- geocel/settings.py
+++ geocel/settings.py
@@ -312,14 +312,8 @@ SIMPLE_JWT = {
 }
 
 # CORS Settings (for frontend integration)
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',') if os.getenv('CORS_ALLOWED_ORIGINS') else [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "http://localhost:5173",  # Vite default port
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',') 
+
 
 CORS_ALLOW_CREDENTIALS = True
 
